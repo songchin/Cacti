b6710ff78a39a766d33d29fe49500a73bf42e6f6
iberry <iberry@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-10-31 14:01:25 +0000

    fix for bandwidth summation resolution (thanks Hip)

--- trunk/cacti/lib/graph_variables.php
+++ trunk/cacti/lib/graph_variables.php
@@ -78,7 +78,7 @@ function ninety_fifth_percentile($local_data_id, $start_time, $end_time) {
    @arg $ds_steps - how many seconds each period represents
    @returns - (array) an array containing each data source item, and its sum */
 function bandwidth_summation($local_data_id, $start_time, $end_time, $rra_steps, $ds_steps) {
-	$fetch_array = rrdtool_function_fetch($local_data_id, $start_time, $end_time);
+	$fetch_array = rrdtool_function_fetch($local_data_id, $start_time, $end_time, $rra_steps * $ds_steps);
 
 	if ((!isset($fetch_array["data_source_names"])) || (count($fetch_array["data_source_names"]) == 0)) {
 		return;
