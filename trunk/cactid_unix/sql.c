04c3d49c9be7bd6bc2d7731f5c45ee5c09bc1075
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-11-01 15:55:25 +0000

    change to support not able to connect errors with high end polling

--- trunk/cactid_unix/sql.c
+++ trunk/cactid_unix/sql.c
@@ -62,26 +62,38 @@ MYSQL_RES *db_query(MYSQL *mysql, char *query) {
 
 int db_connect(char *database, MYSQL *mysql) {
 	char logmessage[LOGSIZE];
+	int retries;
 
 	if (set.verbose == POLLER_VERBOSITY_DEBUG) {
 		snprintf(logmessage, LOGSIZE, "MYSQL: Connecting to MySQL database '%s' on '%s'...\n", database, set.dbhost);
 		cacti_log(logmessage);
 	}
 
-	thread_mutex_lock(LOCK_MYSQL);
-	mysql_init(mysql);
+	retries = 1;
 
-	if (!mysql_real_connect(mysql, set.dbhost, set.dbuser, set.dbpass, database, 0, NULL, 0)) {
-		snprintf(logmessage, LOGSIZE, "MYSQL: Connection Failed: %s\n", mysql_error(mysql));
-		cacti_log(logmessage);
-		thread_mutex_unlock(LOCK_MYSQL);
-		exit(0);
-	}else{
-		thread_mutex_unlock(LOCK_MYSQL);
-		return (0);
+	while (1) {
+		thread_mutex_lock(LOCK_MYSQL);
+		mysql_init(mysql);
+
+		if (!mysql_real_connect(mysql, set.dbhost, set.dbuser, set.dbpass, database, 0, NULL, 0)) {
+			if (retries > 20) {
+				snprintf(logmessage, LOGSIZE, "MYSQL: Connection failed after 10 attempts : %s\n", mysql_error(mysql));
+				cacti_log(logmessage);
+				thread_mutex_unlock(LOCK_MYSQL);
+				exit(-1);
+			} else {
+				retries++;
+				usleep(100000);
+			}
+		}else {
+			thread_mutex_unlock(LOCK_MYSQL);
+			return (0);
+		}
 	}
 }
 
 void db_disconnect(MYSQL *mysql) {
 	mysql_close(mysql);
 }
+
+
