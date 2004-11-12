d5306141ed1e9a144ded5d4d258a88beccc2e0d1
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-11-12 04:18:07 +0000

    initial version just starting the file and naming a few functions

---
+++ trunk/cacti/lib/api_user.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004 Ian Berry                                            |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | cacti: a php-based graphing solution                                    |
+ +-------------------------------------------------------------------------+
+ | Most of this code has been designed, written and is maintained by       |
+ | Ian Berry. See about.php for specific developer credit. Any questions   |
+ | or comments regarding this code should be directed to:                  |
+ | - iberry@raxnet.net                                                     |
+ +-------------------------------------------------------------------------+
+ | - raXnet - http://www.raxnet.net/                                       |
+ +-------------------------------------------------------------------------+
+*/
+
+/* this file is in heavy flux, lots of functions will be written 
+as the interace is moved over to api format */
+
+function api_user_add() {
+
+}
+
+function api_user_remove() {
+
+}
+
+function api_user_edit() {
+
+}
+
+function api_user_enable() {
+
+}
+
+function api_user_disable() {
+
+}
+
+function api_user_copy() {
+
+}
+
+function api_user_info() {
+
+}
+
+
+
+
+
