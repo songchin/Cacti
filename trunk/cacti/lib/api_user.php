4359d83422fd2463834883df504e9901bb245b3a
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-11-13 17:17:39 +0000

    more functions added

--- trunk/cacti/lib/api_user.php
+++ trunk/cacti/lib/api_user.php
@@ -24,36 +24,157 @@
  +-------------------------------------------------------------------------+
 */
 
-/* this file is in heavy flux, lots of functions will be written 
-as the interace is moved over to api format */
+/*
+To use this api file you must include
 
+include/config.php
+
+*/
+
+/* coming soon */
 function api_user_add() {
 
 }
-
-function api_user_remove() {
-
-}
-
+/* coming soon */
 function api_user_edit() {
 
 }
 
-function api_user_enable() {
+/* api_user_changepassword - changes users password, old password is optional
+   @arg $user_id - User id to change password for
+   @arg $password_new - New password to set for user 
+   @arg $password_old - Old password, optional, if passed will validate 
+   @returns - '0' is success, '1' is general error, '2' fail password authentication,
+              '3' user not found */
+function api_user_changepassword($user_id, $password_new, $password_old="") {
+
+	/* validate old password */
+	if (!empty($password_old)) {
+		if (!sizeof(db_fetch_row("select id from user_auth where id ='" . $user_id . "' and password = '" . md5($password_old) . "' and realm = 0"))) {
+			/* Password validation error */
+			return 2;	
+		}
+	}
+
+	/* validate user exists */
+	if (sizeof(db_fetch_row("select id from user_auth where id ='" . $user_id . "' and realm = 0"))) {
+		if (db_execute("update user_auth set password = '" . md5($password_new) . "' where id = '" . $user_id . "'") == 1) {
+			/* password changed */
+			return 0;
+		}else{
+			/* error */
+			return 1;
+		}
+	}else{
+		/* user not found */
+		return 3;
+	}
+
+	/* all else fails return error */
+	return 1;
 
 }
 
-function api_user_disable() {
+/* api_user_info - returns an array of value based on request array
+   @arg $array  - Array of vales to query for example:
+		array( "username" => "admin")
+*/
+/* still not working */
+function api_user_info($array) {
+
+	/* build SQL query */
+	$sql_query = "SELECT * FROM auth_user WHERE ";
+	$sql_where = "";
+	if (sizeof($array) > 0) {
+		foreach ($array as $field => $value) {
+			$sql_where .= $field . " = '" . $value . "' AND ";
+		}
+		/* remove trailing AND */
+		
+	}else{
+		/* error no array */
+		return "";
+	}
 
 }
 
-function api_user_copy() {
+/* api_user_remove - removes a user account
+   @arg $user_id - user id */
+function api_user_remove($user_id) {
 
+	if (!empty($user_id)) {
+		if ($user_id != 1) {
+			db_execute("delete from user_auth where id = '" . $user_id . "'");
+			db_execute("delete from user_auth_realm where user_id = '" . $user_id . "'");
+			db_execute("delete from user_auth_perms where user_id = '" . $user_id . "'");
+			db_execute("delete from settings_graphs where user_id = '" . $user_id . "'");
+		}
+	}
 }
 
-function api_user_info() {
 
+/* api_user_enable - enables  a user account
+   @arg $user_id - user id */
+function api_user_enable($user_id) {
+        if (!empty($user_id)) {
+                if ($user_id != 1) {
+                        db_execute("update user_auth set enabled = 1 where id=" . $user_id);
+		}
+	}
 }
+
+
+/* api_user_disable - disables a user account
+   @arg $user_id - user id */
+function api_user_disable($user_id) {
+        if (!empty($user_id)) {
+                if ($user_id != 1) {
+                        db_execute("update user_auth set enabled = 0 where id=" . $user_id);
+		}
+	}
+}
+
+
+/* api_user_copy - copies a user account
+   @arg $template_user - username of account that should be used as the template
+   @arg $new_user - username of the account to be created
+   @arg $new_realm - the realm the new account should be a member of */
+function api_user_copy($template_user, $new_user, $new_realm=-1) {
+        $user_auth = db_fetch_row("select * from user_auth where username = '$template_user'");
+        $user_auth['username'] = $new_user;
+	if ($new_realm != -1) {
+		$user_auth['realm'] = $new_realm;
+        }
+	$old_id = $user_auth['id'];
+        $user_auth['id'] = 0;
+
+        $new_id = sql_save($user_auth, 'user_auth');
+
+        $user_auth_perms = db_fetch_assoc("select * from user_auth_perms where user_id = '$old_id'");
+        foreach ($user_auth_perms as $user_auth_perm) {
+                $user_auth_perm['user_id'] = $new_id;
+                sql_save($user_auth_perm, 'user_auth_perms', array('user_id', 'item_id', 'type'));
+        }
+
+        $user_auth_realm = db_fetch_assoc("select * from user_auth_realm where user_id = '$old_id'");
+        foreach ($user_auth_realm as $row) {
+                $row['user_id'] = $new_id;
+                sql_save($row, 'user_auth_realm', array('realm_id', 'user_id'));
+        }
+
+        $settings_graphs = db_fetch_assoc("select * from settings_graphs where user_id = '$old_id'");
+        foreach ($settings_graphs as $settings_graph) {
+                $settings_graph['user_id'] = $new_id;
+                sql_save($settings_graph, 'settings_graphs', array('user_id', 'name'));
+        }
+
+        $settings_tree = db_fetch_assoc("select * from settings_tree where user_id = '$old_id'");
+        foreach ($settings_tree as $row) {
+                $row['user_id'] = $new_id;
+                sql_save($settings_tree, 'settings_tree', array('user_id', 'graph_tree_item_id'));
+        }
+}
+
 
 
 
