5882749d8626ad1b560a79778c8d8aca09bbd829
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-11-24 18:37:29 +0000

    reorder functions into sections

--- trunk/cacti/lib/api_user.php
+++ trunk/cacti/lib/api_user.php
@@ -38,295 +38,11 @@ $graph_perms_type_array = array(
 	"graph_template" => "4"
 	);
 
-
-
-
-/* api_user_graph_settings_save
-  @arg $user_id - user id
-  @arg $array - Array containing values to save to the database. fieldname => value
-  @return - '0' success, '1' error */
-function api_user_graph_setting_save($user_id,$array) {
-	global $settings_graphs;
-
-	/* prevent array squashing */
-	$settings_graphs_local = $settings_graphs;
-
-	/* validation */
-	if (empty($user_id)) {
-		return 1;
-	}
-
-	/* build array of values */
-	$return_array = array();
-
-	/* go through form sections */
-	while (list($tab_short_name, $tab_fields) = each($settings_graphs_local)) {
-		/* process fields */
-		while (list($field_name, $field_array) = each($tab_fields)) {
-			if ((isset($field_array["items"])) && (is_array($field_array["items"]))) {
-				/* sub fields detected */
-				while (list($sub_field_name, $sub_field_array) = each($field_array["items"])) {
-					db_execute("replace into settings_graphs (user_id,name,value) values (" . $user_id . ",'" . $sub_field_name . "', '" . (isset($array[$sub_field_name]) ? $array[$sub_field_name] : "") . "')");
-				}
-			}else{
-				/* normal field */
-				db_execute("replace into settings_graphs (user_id,name,value) values (" . $user_id . ",'$field_name', '" . (isset($array[$field_name]) ? $array[$field_name] : "") . "')");
-			}
-		}
-	}
-	
-	return 0;
-
-
-}
-
-/* api_user_graph_setting_list
-  @arg $user_id - user id 
-  @return - array of field => value
+/*
+########################################
+# user functions
+########################################
 */
-function api_user_graph_setting_list($user_id) {
-	global $settings_graphs;
-
-	/* prevent array squashing */
-	$settings_graphs_local = $settings_graphs;
-
-	/* Get settings from database */
-	$user_settings = array();
-	if (!empty($user_id)) {
-		$setting = db_fetch_assoc("select name,value from settings_graphs where user_id = '" . $user_id . "'");
-		while (list($record, $fields) = each($setting)) {
-			$user_settings[$fields["name"]] = $fields["value"];	
-		}
-	}
-
-	/* build array of values */
-	$return_array = array();
-
-	/* go through form sections */
-	while (list($tab_short_name, $tab_fields) = each($settings_graphs_local)) {
-		/* process fields */
-		while (list($field_name, $field_array) = each($tab_fields)) {
-			if ((isset($field_array["items"])) && (is_array($field_array["items"]))) {
-				/* sub fields detected */
-				while (list($sub_field_name, $sub_field_array) = each($field_array["items"])) {
-					if (isset($user_settings[$sub_field_name])) {
-						$return_array[$sub_field_name] = $user_settings[$sub_field_name];
-					}else{
-						$return_array[$sub_field_name] = $sub_field_array["default"];
-					}
-				}
-			}else{
-				/* regular field */
-				if (isset($user_settings[$field_name])) {
-					$return_array[$field_name] = $user_settings[$field_name];
-				}else{
-					$return_array[$field_name] = $field_array["default"];
-				}
-			}
-		}
-	}
-
-	return $return_array;
-	
-}
-
-/* api_user_graph_perms_list 
-   @arg $type - type of perms to look at.
-   @arg $user_id - User ID to query values for
-   @return - Array of values: id, name */
-function api_user_graph_perms_list($type,$user_id) {
-	global $graph_perms_type_array;
-
-	/* validation */
-	if (empty($user_id)) {
-		$user_id = 0;
-	}
-
-	switch ($graph_perms_type_array[$type]) {
-
-	case "1":
-
-		$return_array = db_fetch_assoc("select 
-			graph_templates_graph.local_graph_id,
-			graph_templates_graph.title_cache
-			from graph_templates_graph
-			left join user_auth_perms on (graph_templates_graph.local_graph_id=user_auth_perms.item_id and user_auth_perms.type=1)
-			where graph_templates_graph.local_graph_id > 0
-			and user_auth_perms.user_id = '" . $user_id . "'
-			order by graph_templates_graph.title_cache");
-		break;
-
-	case "2":
-
-		$return_array = db_fetch_assoc("select
-			graph_tree.id,
-			graph_tree.name
-			from graph_tree
-			left join user_auth_perms on (graph_tree.id=user_auth_perms.item_id and user_auth_perms.type=2)
-			where user_auth_perms.user_id = '" . $user_id . "'
-			order by graph_tree.name");
-
-		break;
-
-	case "3":
-
-		$return_array = db_fetch_assoc("select
-			host.id,
-			CONCAT_WS('',host.description,' (',host.hostname,')') as name
-			from host
-			left join user_auth_perms on (host.id=user_auth_perms.item_id and user_auth_perms.type=3)
-			where user_auth_perms.user_id = '" . $user_id . "'
-			order by host.description,host.hostname");
-
-		break;
-
-	case "4":
-
-		$return_array = db_fetch_assoc("select
-			graph_templates.id,
-			graph_templates.name
-			from graph_templates
-			left join user_auth_perms on (graph_templates.id=user_auth_perms.item_id and user_auth_perms.type=4)
-			where user_auth_perms.user_id = '" . $user_id . "'
-			order by graph_templates.name");
-
-		break;
-	
-	default:
-		$return_array = array();
-
-	}
-
-	return $return_array;
-
-}
-
-/* api_user_save
-   @arg $array - an array containing each column -> value mapping in the user_auth table, always remember the id field
-   @return - id of user saved, new or existing 
-   reference user_admin.php for examples */
-function api_user_save($array) {
-
-	$user_id = sql_save($array, "user_auth");
-
-	return $user_id;
-
-}
-
-/* api_user_realms_list 
-  @arg $user_id - user id
-  @return - Array of indexed by realm_id with a sub array of realm_name and value */
-function api_user_realms_list($user_id) {
-	global $user_auth_realms;
-
-	if (!empty($user_id)) {
-		/* process realms */
-		while (list($realm_id, $realm_name) = each($user_auth_realms)) {
-				
-			if (sizeof(db_fetch_assoc("select realm_id from user_auth_realm where user_id = '" . $user_id . "' and realm_id = '" . $realm_id . "'")) > 0) {
-				$value = "1";
-			}else{
-				$value = "0";
-			}
-			$realm_list[$realm_id] = array(
-				"realm_name" => $realm_name,
-				"value" => $value
-			);
-	
-		}
-		return $realm_list;
-	}else{
-		return "";
-	}
-
-}
-
-/* api_user_realms_save
-  @arg $user_id - user id
-  @arg $array - single dimension array of realm_id that are granted, empty array will clear all realms */
-function api_user_realms_save($user_id,$array) {
-
-	/* validate */
-	if (!empty($user_id)) {
-
-		/* remove any existing permissions */
-		db_execute("delete from user_auth_realm where user_id = '" . $user_id . "'");
-		
-		/* insert the new permission */
-		foreach($array as $realm_id) {
-			db_execute("replace into user_auth_realm (user_id,realm_id) values ('" . $user_id . "','" . $realm_id . "')");
-		}
-	}
-
-}
-
-/* api_user_graph_perms_add 
-  @arg $type - graph, tree, host, graph_template types
-  @arg $user_id - user id
-  @arg $item_id = item id of the type of item, example type = graph id is from graph table */
-function api_user_graph_perms_add($type,$user_id,$item_id) {
-	global $graph_perms_type_array;
-
-	/* validation */
-	if ((!empty($graph_perms_type_array[$type])) && (!empty($user_id)) && (!empty($item_id))) {
-		db_execute("replace into user_auth_perms (user_id,item_id,type) values ('" . $user_id . "','" . $item_id . "','" . $graph_perms_type_array[$type] . "')");
-	}
-
-}
-
-/* api_user_graph_perms_remove 
-  @arg $type - graph, tree, host, graph_template types
-  @arg $user_id - user id 
-  @arg $item_id - item id of type of item, example type = graph id is from graph table */
-function api_user_graph_perms_remove($type,$user_id,$item_id) {
-	global $graph_perms_type_array;
-	
-	/* validation */
-	if ((!empty($graph_perms_type_array[$type])) && (!empty($user_id)) && (!empty($item_id))) {
-		db_execute("delete from user_auth_perms where type = '" . $graph_perms_type_array[$type] . "' and user_id = '" . $user_id . "' and item_id = '" . $item_id . "'");
-	}
-
-}
-
-/* api_user_changepassword - changes users password, old password is optional
-   @arg $user_id - User id to change password for
-   @arg $password_new - New password to set for user 
-   @arg $password_old - Old password, optional, if passed will validate 
-   @returns - '0' is success, '1' is general error, '2' fail password authentication,
-              '3' user not found  */
-function api_user_changepassword($user_id, $password_new, $password_old="") {
-
-	/* validate we can change the password */
-	if (read_config_option("auth_method") != "1") {
-		return 1;
-	}
-
-	/* validate old password */
-	if (!empty($password_old)) {
-		if (!sizeof(db_fetch_row("select id from user_auth where id ='" . $user_id . "' and password = '" . md5($password_old) . "' and realm = 0"))) {
-			/* Password validation error */
-			return 2;	
-		}
-	}
-
-	/* validate user exists */
-	if (sizeof(db_fetch_row("select id from user_auth where id ='" . $user_id . "' and realm = 0"))) {
-		if (db_execute("update user_auth set password = '" . md5($password_new) . "',must_change_password = '' where id = '" . $user_id . "'") == 1) {
-			/* password changed */
-			return 0;
-		}else{
-			/* error */
-			return 1;
-		}
-	}else{
-		/* user not found */
-		return 3;
-	}
-
-	/* all else fails return error */
-	return 1;
-
-}
 
 /* api_user_list - returns an array of users on the system
    @arg $array  - Array of columns to sort by for example:
@@ -341,7 +57,7 @@ function api_user_list($array) {
 		foreach ($array as $field => $value) {
 			$sql_sort .= $value . ", ";
 		}
-		/* remove trailing AND */
+		/* remove trailing comma */
 		$sql_sort = preg_replace("/\,\ $/", "", $sql_sort);
 		$sql_query = $sql_query . $sql_sort;
 	}else{
@@ -385,6 +101,58 @@ function api_user_info($array) {
 	$user["ip"] = $last_login["ip"];
 
 	return $user;
+
+}
+
+/* api_user_save
+   @arg $array - an array containing each column -> value mapping in the user_auth table, always remember the id field
+   @return - id of user saved, new or existing 
+   reference user_admin.php for examples */
+function api_user_save($array) {
+
+	$user_id = sql_save($array, "user_auth");
+
+	return $user_id;
+
+}
+
+/* api_user_changepassword - changes users password, old password is optional
+   @arg $user_id - User id to change password for
+   @arg $password_new - New password to set for user 
+   @arg $password_old - Old password, optional, if passed will validate 
+   @returns - '0' is success, '1' is general error, '2' fail password authentication,
+              '3' user not found  */
+function api_user_changepassword($user_id, $password_new, $password_old="") {
+
+	/* validate we can change the password */
+	if (read_config_option("auth_method") != "1") {
+		return 1;
+	}
+
+	/* validate old password */
+	if (!empty($password_old)) {
+		if (!sizeof(db_fetch_row("select id from user_auth where id ='" . $user_id . "' and password = '" . md5($password_old) . "' and realm = 0"))) {
+			/* Password validation error */
+			return 2;	
+		}
+	}
+
+	/* validate user exists */
+	if (sizeof(db_fetch_row("select id from user_auth where id ='" . $user_id . "' and realm = 0"))) {
+		if (db_execute("update user_auth set password = '" . md5($password_new) . "',must_change_password = '' where id = '" . $user_id . "'") == 1) {
+			/* password changed */
+			return 0;
+		}else{
+			/* error */
+			return 1;
+		}
+	}else{
+		/* user not found */
+		return 3;
+	}
+
+	/* all else fails return error */
+	return 1;
 
 }
 
@@ -471,6 +239,259 @@ function api_user_copy($template_user, $new_user, $new_realm=-1) {
         }
 
 	return 0;
+}
+
+/*
+########################################
+# user realm (permissions) functions
+########################################
+*/
+
+/* api_user_realms_list 
+  @arg $user_id - user id
+  @return - Array of indexed by realm_id with a sub array of realm_name and value */
+function api_user_realms_list($user_id) {
+	global $user_auth_realms;
+
+	if (!empty($user_id)) {
+		/* process realms */
+		while (list($realm_id, $realm_name) = each($user_auth_realms)) {
+				
+			if (sizeof(db_fetch_assoc("select realm_id from user_auth_realm where user_id = '" . $user_id . "' and realm_id = '" . $realm_id . "'")) > 0) {
+				$value = "1";
+			}else{
+				$value = "0";
+			}
+			$realm_list[$realm_id] = array(
+				"realm_name" => $realm_name,
+				"value" => $value
+			);
+	
+		}
+		return $realm_list;
+	}else{
+		return "";
+	}
+
+}
+
+/* api_user_realms_save
+  @arg $user_id - user id
+  @arg $array - single dimension array of realm_id that are granted, empty array will clear all realms */
+function api_user_realms_save($user_id,$array) {
+
+	/* validate */
+	if (!empty($user_id)) {
+
+		/* remove any existing permissions */
+		db_execute("delete from user_auth_realm where user_id = '" . $user_id . "'");
+		
+		/* insert the new permission */
+		foreach($array as $realm_id) {
+			db_execute("replace into user_auth_realm (user_id,realm_id) values ('" . $user_id . "','" . $realm_id . "')");
+		}
+	}
+
+}
+
+/*
+########################################
+# graph settings functions
+########################################
+*/
+
+/* api_user_graph_setting_list
+  @arg $user_id - user id 
+  @return - array of field => value
+*/
+function api_user_graph_setting_list($user_id) {
+	global $settings_graphs;
+
+	/* prevent array squashing */
+	$settings_graphs_local = $settings_graphs;
+
+	/* Get settings from database */
+	$user_settings = array();
+	if (!empty($user_id)) {
+		$setting = db_fetch_assoc("select name,value from settings_graphs where user_id = '" . $user_id . "'");
+		while (list($record, $fields) = each($setting)) {
+			$user_settings[$fields["name"]] = $fields["value"];	
+		}
+	}
+
+	/* build array of values */
+	$return_array = array();
+
+	/* go through form sections */
+	while (list($tab_short_name, $tab_fields) = each($settings_graphs_local)) {
+		/* process fields */
+		while (list($field_name, $field_array) = each($tab_fields)) {
+			if ((isset($field_array["items"])) && (is_array($field_array["items"]))) {
+				/* sub fields detected */
+				while (list($sub_field_name, $sub_field_array) = each($field_array["items"])) {
+					if (isset($user_settings[$sub_field_name])) {
+						$return_array[$sub_field_name] = $user_settings[$sub_field_name];
+					}else{
+						$return_array[$sub_field_name] = $sub_field_array["default"];
+					}
+				}
+			}else{
+				/* regular field */
+				if (isset($user_settings[$field_name])) {
+					$return_array[$field_name] = $user_settings[$field_name];
+				}else{
+					$return_array[$field_name] = $field_array["default"];
+				}
+			}
+		}
+	}
+
+	return $return_array;
+	
+}
+
+/* api_user_graph_settings_save
+  @arg $user_id - user id
+  @arg $array - Array containing values to save to the database. fieldname => value
+  @return - '0' success, '1' error */
+function api_user_graph_setting_save($user_id,$array) {
+	global $settings_graphs;
+
+	/* prevent array squashing */
+	$settings_graphs_local = $settings_graphs;
+
+	/* validation */
+	if (empty($user_id)) {
+		return 1;
+	}
+
+	/* build array of values */
+	$return_array = array();
+
+	/* go through form sections */
+	while (list($tab_short_name, $tab_fields) = each($settings_graphs_local)) {
+		/* process fields */
+		while (list($field_name, $field_array) = each($tab_fields)) {
+			if ((isset($field_array["items"])) && (is_array($field_array["items"]))) {
+				/* sub fields detected */
+				while (list($sub_field_name, $sub_field_array) = each($field_array["items"])) {
+					db_execute("replace into settings_graphs (user_id,name,value) values (" . $user_id . ",'" . $sub_field_name . "', '" . (isset($array[$sub_field_name]) ? $array[$sub_field_name] : "") . "')");
+				}
+			}else{
+				/* normal field */
+				db_execute("replace into settings_graphs (user_id,name,value) values (" . $user_id . ",'$field_name', '" . (isset($array[$field_name]) ? $array[$field_name] : "") . "')");
+			}
+		}
+	}
+	
+	return 0;
+
+
+}
+
+/*
+########################################
+# graph permission functions
+########################################
+*/
+
+/* api_user_graph_perms_list 
+   @arg $type - type of perms to look at.
+   @arg $user_id - User ID to query values for
+   @return - Array of values: id, name */
+function api_user_graph_perms_list($type,$user_id) {
+	global $graph_perms_type_array;
+
+	/* validation */
+	if (empty($user_id)) {
+		$user_id = 0;
+	}
+
+	switch ($graph_perms_type_array[$type]) {
+
+	case "1":
+
+		$return_array = db_fetch_assoc("select 
+			graph_templates_graph.local_graph_id,
+			graph_templates_graph.title_cache
+			from graph_templates_graph
+			left join user_auth_perms on (graph_templates_graph.local_graph_id=user_auth_perms.item_id and user_auth_perms.type=1)
+			where graph_templates_graph.local_graph_id > 0
+			and user_auth_perms.user_id = '" . $user_id . "'
+			order by graph_templates_graph.title_cache");
+		break;
+
+	case "2":
+
+		$return_array = db_fetch_assoc("select
+			graph_tree.id,
+			graph_tree.name
+			from graph_tree
+			left join user_auth_perms on (graph_tree.id=user_auth_perms.item_id and user_auth_perms.type=2)
+			where user_auth_perms.user_id = '" . $user_id . "'
+			order by graph_tree.name");
+
+		break;
+
+	case "3":
+
+		$return_array = db_fetch_assoc("select
+			host.id,
+			CONCAT_WS('',host.description,' (',host.hostname,')') as name
+			from host
+			left join user_auth_perms on (host.id=user_auth_perms.item_id and user_auth_perms.type=3)
+			where user_auth_perms.user_id = '" . $user_id . "'
+			order by host.description,host.hostname");
+
+		break;
+
+	case "4":
+
+		$return_array = db_fetch_assoc("select
+			graph_templates.id,
+			graph_templates.name
+			from graph_templates
+			left join user_auth_perms on (graph_templates.id=user_auth_perms.item_id and user_auth_perms.type=4)
+			where user_auth_perms.user_id = '" . $user_id . "'
+			order by graph_templates.name");
+
+		break;
+	
+	default:
+		$return_array = array();
+
+	}
+
+	return $return_array;
+
+}
+
+/* api_user_graph_perms_add 
+  @arg $type - graph, tree, host, graph_template types
+  @arg $user_id - user id
+  @arg $item_id = item id of the type of item, example type = graph id is from graph table */
+function api_user_graph_perms_add($type,$user_id,$item_id) {
+	global $graph_perms_type_array;
+
+	/* validation */
+	if ((!empty($graph_perms_type_array[$type])) && (!empty($user_id)) && (!empty($item_id))) {
+		db_execute("replace into user_auth_perms (user_id,item_id,type) values ('" . $user_id . "','" . $item_id . "','" . $graph_perms_type_array[$type] . "')");
+	}
+
+}
+
+/* api_user_graph_perms_remove 
+  @arg $type - graph, tree, host, graph_template types
+  @arg $user_id - user id 
+  @arg $item_id - item id of type of item, example type = graph id is from graph table */
+function api_user_graph_perms_remove($type,$user_id,$item_id) {
+	global $graph_perms_type_array;
+	
+	/* validation */
+	if ((!empty($graph_perms_type_array[$type])) && (!empty($user_id)) && (!empty($item_id))) {
+		db_execute("delete from user_auth_perms where type = '" . $graph_perms_type_array[$type] . "' and user_id = '" . $user_id . "' and item_id = '" . $item_id . "'");
+	}
+
 }
 
 ?>
