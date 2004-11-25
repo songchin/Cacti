5211e5aef18394a97f4693389de6470505f1303b
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-11-25 19:03:25 +0000

    fixed bug where guest user could change the password if access directly

--- trunk/cacti/auth_changepassword.php
+++ trunk/cacti/auth_changepassword.php
@@ -27,7 +27,16 @@
 include("./include/config.php");
 include("./lib/api_user.php");
 
-$user = db_fetch_row("select * from user_auth where id=" . $_SESSION["sess_user_id"]);
+#$user = db_fetch_row("select * from user_auth where id=" . $_SESSION["sess_user_id"]);
+$user = api_user_info( array( "id" => $_SESSION["sess_user_id"] ) );
+
+/* find out if we are logged in as a 'guest user' or not */
+$using_guest_account = false;
+if (read_config_option("guest_user") != "0") {
+	if ($user["username"] == read_config_option("guest_user")) {
+		$using_guest_account = true;
+	}
+}
 
 /* default to !bad_password */
 $bad_password = false;
@@ -36,47 +45,52 @@ $old_password = false;
 /* set default action */
 if (!isset($_REQUEST["action"])) { $_REQUEST["action"] = ""; }
 
-switch ($_REQUEST["action"]) {
-case 'changepassword':
+if (!$using_guest_account) {
+
+	switch ($_REQUEST["action"]) {
+	case 'changepassword':
 	
-	if (db_fetch_cell("select password from user_auth where id=" . $_SESSION["sess_user_id"]) == md5($_POST["password"])) {
-		$old_password = true;
-	}else{
-		if (($_POST["password"] == $_POST["confirm"]) && ($_POST["password"] != "")) {
-
-			/* Log password change */
-			db_execute("insert into user_log (user_id,username,time,result,ip) values('" . $_SESSION["sess_user_id"] . "','" . $user["username"] . "',NOW(),3,'" . $_SERVER["REMOTE_ADDR"] . "')");
-
-			/* change password */
-			api_user_changepassword($_SESSION["sess_user_id"], $_POST["password"]);
-	
-			kill_session_var("sess_change_password");
-
-			/* ok, at the point the user has been sucessfully authenticated; so we must
-			decide what to do next */
-	
-			/* if no console permissions show graphs otherwise, pay attention to user setting */
-			$realm_id = $user_auth_realm_filenames["index.php"];
-
-			if (sizeof(db_fetch_assoc("select user_auth_realm.realm_id from user_auth_realm where user_auth_realm.user_id = '" . $_SESSION["sess_user_id"] . "' and user_auth_realm.realm_id = '" . $realm_id . "'")) > 0) {
-				switch ($user["login_opts"]) {
-					case '1': /* referer */
-						header("Location: " . $_POST["ref"]); break;
-					case '2': /* default console page */
-						header("Location: index.php"); break;
-					case '3': /* default graph page */
-						header("Location: graph_view.php"); break;
-				}
-			}else{
-				header("Location: graph_view.php");
-			}
-
-			exit;
+		
+		if (db_fetch_cell("select password from user_auth where id=" . $_SESSION["sess_user_id"]) == md5($_POST["password"])) {
+			$old_password = true;
 		}else{
-			$bad_password = true;
+			if (($_POST["password"] == $_POST["confirm"]) && ($_POST["password"] != "")) {
+
+				/* Log password change */
+				db_execute("insert into user_log (user_id,username,time,result,ip) values('" . $_SESSION["sess_user_id"] . "','" . $user["username"] . "',NOW(),3,'" . $_SERVER["REMOTE_ADDR"] . "')");
+	
+				/* change password */
+				api_user_changepassword($_SESSION["sess_user_id"], $_POST["password"]);
+		
+				kill_session_var("sess_change_password");
+
+				/* ok, at the point the user has been sucessfully authenticated; so we must
+				decide what to do next */
+	
+				/* if no console permissions show graphs otherwise, pay attention to user setting */
+				$realm_id = $user_auth_realm_filenames["index.php"];
+
+				if (sizeof(db_fetch_assoc("select user_auth_realm.realm_id from user_auth_realm where user_auth_realm.user_id = '" . $_SESSION["sess_user_id"] . "' and user_auth_realm.realm_id = '" . $realm_id . "'")) > 0) {
+					switch ($user["login_opts"]) {
+						case '1': /* referer */
+							header("Location: " . $_POST["ref"]); break;
+						case '2': /* default console page */
+							header("Location: index.php"); break;
+						case '3': /* default graph page */
+							header("Location: graph_view.php"); break;
+					}
+				}else{
+					header("Location: graph_view.php");
+				}
+	
+				exit;
+			}else{
+				$bad_password = true;
+			}
 		}
+		break;
 	}
-	break;
+
 }
 ?>
 <html>
@@ -92,15 +106,24 @@ case 'changepassword':
 	-->
 	</style>
 </head>
+<?php if (!$using_guest_account) { ?>
 
 <body onload="document.login.password.focus()">
 
 <form name="login" method="post" action="<?php print basename($_SERVER["PHP_SELF"]);?>">
 
+<?php } ?>
+
 <table align="center">
 	<tr>
 		<td colspan="2"><img src="<?php print html_get_theme_images_path('auth_login.gif');?>" border="0" alt=""></td>
 	</tr>
+<?php if ($using_guest_account) { ?>
+	<tr height="10"><td></td></tr>
+	<tr>
+		<td colspan="2" align="center"><font color="#FF0000" size="+2"><strong>Access Denied</strong></font></td>
+	</tr>
+<?php }else{ ?>
 	<?php if ($bad_password == true) {?>
 	<tr height="10"><td></td></tr>
 	<tr>
@@ -134,12 +157,16 @@ case 'changepassword':
 	<tr>
 		<td><input type="submit" value="Save"></td>
 	</tr>
+
+<?php } ?>
 </table>
 
+
+<?php if (!$using_guest_account) { ?>
 <input type="hidden" name="action" value="changepassword">
 <input type="hidden" name="ref" value="<?php print $_REQUEST["ref"];?>">
 
 </form>
-
+<?php } ?>
 </body>
 </html>
