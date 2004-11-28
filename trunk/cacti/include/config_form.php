21ee42277bf5099ce2add90d428b3fddc26abad4
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-11-28 04:18:19 +0000

    changed friendly name on user edit screen

--- trunk/cacti/include/config_form.php
+++ trunk/cacti/include/config_form.php
@@ -1130,7 +1130,7 @@ $fields_user_user_edit_host = array(
 		),
 	"password_expire_length" => array(
 		"method" => "drop_array",
-		"friendly_name" => "Password Expiration",
+		"friendly_name" => "Password Expiration Interval",
 		"description" => "How often the users password will expire and a password change will be forced.",
 		"value" => "|arg1:password_expire_length|",
 		"array" => $user_password_expire_intervals,
