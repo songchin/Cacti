378dfacec5db28aedbda523e8656e4b6ab2a9dbd
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-12-04 21:42:57 +0000

    Reformat the table updates to identify featues based changes

--- trunk/cacti/install/0_8_6b_to_0_8_7.php
+++ trunk/cacti/install/0_8_6b_to_0_8_7.php
@@ -26,25 +26,37 @@
 
 function upgrade_to_0_8_7() {
 	// ALTER TABLE `data_input_fields` DROP `sequence`;
+
+	/* Format bug from 0.8.6x */
 	db_install_execute("0.8.7", "ALTER TABLE `host` CHANGE `min_time` `min_time` decimal(9,5) default '99999.99', CHANGE `max_time` `max_time` decimal(9,5) default '0.00000', CHANGE `cur_time` `cur_time` decimal(9,5) default '0.00000', CHANGE `avg_time` `avg_time` decimal(9,5) default '0.00000';");
+
+	/* Host Specific Availability Checking Feature */
 	db_install_execute("0.8.7", "ALTER TABLE `host` ADD `poller_id` smallint(5) unsigned NOT NULL default '0' AFTER `id`;");
 	db_install_execute("0.8.7", "ALTER TABLE `host` ADD `availability_method` smallint(5) unsigned NOT NULL default '1' AFTER `snmp_timeout`;");
 	db_install_execute("0.8.7", "ALTER TABLE `host` ADD `ping_method` smallint(5) unsigned default '' AFTER `availability_method`;");
-	db_install_execute("0.8.7", "ALTER TABLE `host` CHANGE `snmp_username` `snmpv3_auth_username` varchar(50), CHANGE `snmp_password` `snmpv3_auth_password` varchar(50);");
-	db_install_execute("0.8.7", "ALTER TABLE `host` ADD `snmpv3_auth_protocol` varchar(5) AFTER `snmpv3_auth_password`, ADD `snmpv3_priv_passphrase` varchar(200) AFTER `snmpv3_auth_protocol`, ADD `snmpv3_priv_protocol` varchar(5) AFTER `snmpv3_priv_passphrase`;");
+
+	/* Start the Creation of Reserved/Hidden Items in Tables */
 	db_install_execute("0.8.7", "ALTER TABLE `data_input` ADD `reserved` tinyint unsigned NOT NULL default '0' AFTER `id`;");
 	db_install_execute("0.8.7", "UPDATE `data_input` set reserved = '1' where hash = '3eb92bb845b9660a7445cf9740726522' or hash = 'bf566c869ac6443b0c75d1c32b5a350e' or hash = '80e9e4c4191a5da189ae26d0e237f015' or hash = '332111d8b54ac8ce939af87a7eac0c06';");
+
+	/* Multi Poller Architecture Changes */
 	db_install_execute("0.8.7", "ALTER TABLE `poller` ADD `active` varchar(5) default 'On' AFTER `id`;");
 	db_install_execute("0.8.7", "ALTER TABLE `poller` CHANGE `ip_address` `name` varchar(150) NOT NULL default 'Description';");
 	db_install_execute("0.8.7", "ALTER TABLE `poller_item` ADD `availability_method` smallint(5) unsigned NOT NULL default '1' AFTER `snmp_timeout`;");
 	db_install_execute("0.8.7", "ALTER TABLE `poller_item` ADD `ping_method` smallint(5) unsigned default '' AFTER `availability_method`;");
-	db_install_execute("0.8.7", "ALTER TABLE `poller_item` CHANGE `snmp_username` `snmpv3_auth_username` varchar(50), CHANGE `snmp_password` `snmpv3_auth_password` varchar(50);");
-	db_install_execute("0.8.7", "ALTER TABLE `poller_item` ADD `snmpv3_auth_protocol` varchar(5) AFTER `snmpv3_auth_password`, ADD `snmpv3_priv_passphrase` varchar(200) AFTER `snmpv3_auth_protocol`, ADD `snmpv3_priv_protocol` varchar(5) AFTER `snmpv3_priv_passphrase`;");
+
+	/* New Graph Template Options */
 	db_install_execute("0.8.7", "ALTER TABLE `graph_templates_graph` ADD `t_x_grid` char(2) default '0' AFTER `width`, ADD `x_grid` varchar(50) default NULL AFTER `t_x_grid`;");
 	db_install_execute("0.8.7", "ALTER TABLE `graph_templates_graph` ADD `t_y_grid` char(2) default '0' AFTER `x_grid`, ADD `y_grid` varchar(50) default NULL AFTER `t_y_grid`;");
 	db_install_execute("0.8.7", "ALTER TABLE `graph_templates_graph` ADD `t_y_grid_alt` char(2) default '0' AFTER `y_grid`, ADD `y_grid_alt` char(2) default NULL AFTER `t_y_grid_alt`;");
 	db_install_execute("0.8.7", "ALTER TABLE `graph_templates_graph` ADD `t_no_minor` char(2) default '0' AFTER `y_grid_alt`, ADD `no_minor` char(2) default NULL AFTER `t_no_minor`;");
 	db_install_execute("0.8.7", "ALTER TABLE `graph_templates_graph` ADD `t_unit_length` char(2) default '0' AFTER `unit_value`, ADD `unit_length` varchar(5) default '' AFTER `t_unit_length`;");
+
+	/* snmpv3 Support Added */
+	db_install_execute("0.8.7", "ALTER TABLE `host` CHANGE `snmp_username` `snmpv3_auth_username` varchar(50), CHANGE `snmp_password` `snmpv3_auth_password` varchar(50);");
+	db_install_execute("0.8.7", "ALTER TABLE `host` ADD `snmpv3_auth_protocol` varchar(5) AFTER `snmpv3_auth_password`, ADD `snmpv3_priv_passphrase` varchar(200) AFTER `snmpv3_auth_protocol`, ADD `snmpv3_priv_protocol` varchar(5) AFTER `snmpv3_priv_passphrase`;");
+	db_install_execute("0.8.7", "ALTER TABLE `poller_item` CHANGE `snmp_username` `snmpv3_auth_username` varchar(50), CHANGE `snmp_password` `snmpv3_auth_password` varchar(50);");
+	db_install_execute("0.8.7", "ALTER TABLE `poller_item` ADD `snmpv3_auth_protocol` varchar(5) AFTER `snmpv3_auth_password`, ADD `snmpv3_priv_passphrase` varchar(200) AFTER `snmpv3_auth_protocol`, ADD `snmpv3_priv_protocol` varchar(5) AFTER `snmpv3_priv_passphrase`;");
 	db_install_execute("0.8.7", "UPDATE `data_input_fields` SET `name` = 'SNMP Authorization Password (v3)' WHERE `name` = 'SNMP Password';");
 	db_install_execute("0.8.7", "UPDATE `data_input_fields` SET `name` = 'SNMP Authorization Username (v3)' WHERE `name` = 'SNMP Username';");
 	db_install_execute("0.8.7", "UPDATE `data_input_fields` SET `data_name` = 'snmpv3_auth_username' WHERE `data_name` = 'snmp_username';");
@@ -57,7 +69,6 @@ function upgrade_to_0_8_7() {
 	db_indtall_execute("0.8.7", "INSERT INTO `data_input_fields` FIELDS (hash,data_input_id,name,data_name,input_output,update_rra,sequence,type_code,regexp_match,allow_nulls) VALUES ('aa9632293ac20ecd87f5e4691fc244f6','2','SNMP Authorization Protocol (v3)','snmpv3_auth_protocol','in','','0','snmpv3_auth_protocol','','on';");
 	db_indtall_execute("0.8.7", "INSERT INTO `data_input_fields` FIELDS (hash,data_input_id,name,data_name,input_output,update_rra,sequence,type_code,regexp_match,allow_nulls) VALUES ('b9a06e0ff7c042506a0adf013db5a533','2','SNMP Privacy Passphrase (v3)','snmpv3_priv_passphrase','in','','0','snmpv3_priv_passphrase','','on';");
 	db_indtall_execute("0.8.7", "INSERT INTO `data_input_fields` FIELDS (hash,data_input_id,name,data_name,input_output,update_rra,sequence,type_code,regexp_match,allow_nulls) VALUES ('7c3011fb886b6345ed761a173dffd120','2','SNMP Privacy Protocol (v3)','snmpv3_priv_protocol','in','','0','snmpv3_priv_protocol','','on';");
-
 
 	/* Convert to new authentication system */
 	db_install_execute("0.8.7", "ALTER TABLE `user_auth` ADD `enabled` tinyint(1) unsigned NOT NULL default '1', ADD `created` datetime NOT NULL, ADD `password_expire_length` int(4) unsigned NOT NULL default '0', ADD `password_change_last` datetime NOT NULL;");
