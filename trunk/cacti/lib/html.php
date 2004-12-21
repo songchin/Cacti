a8b6d00c273ac48611c3cdbb757b18abe0b0f88a
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-12-21 00:56:25 +0000

    fixed problem with themes if authenication is turned off

--- trunk/cacti/lib/html.php
+++ trunk/cacti/lib/html.php
@@ -247,7 +247,7 @@ function html_header_checkbox($header_items, $form_action = "") {
 /* html_get_theme_css() - returns the Style sheet reference for the current theme
    */
 function html_get_theme_css() {
-	if ((strlen(api_user_theme($_SESSION["sess_user_id"])) > 0) && (api_user_theme($_SESSION["sess_user_id"]) != "default")) {
+	if ((isset($_SESSION["sess_user_id"])) && (api_user_theme($_SESSION["sess_user_id"]) != "default")) {
 		$theme = api_user_theme($_SESSION["sess_user_id"]);
 	}else{
 		$theme = read_config_option("default_theme");
@@ -259,7 +259,7 @@ function html_get_theme_css() {
 /* html_get_theme_image_path() - returns the Style sheet reference for the current theme
    */
 function html_get_theme_images_path($image_file = "") {
-	if ((strlen(api_user_theme($_SESSION["sess_user_id"])) > 0) && (api_user_theme($_SESSION["sess_user_id"]) != "default")) {
+	if ((isset($_SESSION["sess_user_id"])) && (api_user_theme($_SESSION["sess_user_id"]) != "default")) {
 		$theme = api_user_theme($_SESSION["sess_user_id"]);
 	}else{
 		$theme = read_config_option("default_theme");
@@ -279,7 +279,7 @@ function html_get_theme_images_path($image_file = "") {
 function html_theme_color_scheme() {
 	global $config;
 
-	if ((strlen(api_user_theme($_SESSION["sess_user_id"])) > 0) && (api_user_theme($_SESSION["sess_user_id"]) != "default")) {
+	if ((isset($_SESSION["sess_user_id"])) && (api_user_theme($_SESSION["sess_user_id"]) != "default")) {
 		$theme = api_user_theme($_SESSION["sess_user_id"]);
 	}else{
 		$theme = read_config_option("default_theme");
