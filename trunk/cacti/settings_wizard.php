ce31c723da1cb4fabceb026df925381d59f56394
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-12-21 02:44:51 +0000

    update to new format, new ideas

--- trunk/cacti/settings_wizard.php
+++ trunk/cacti/settings_wizard.php
@@ -28,70 +28,154 @@ include("./include/auth.php");
 
 /* settings */
 $wizard_array = array(
+	"general" => array(
+		"friendly_name" => "General",
+		"description" => "General wizard aids you in setting up Cacti's general settings for your enviroment.",
+		"include" => "include/wizards/settings_general.php"
+		),
+	"snmp" => array(
+		"friendly_name" => "SNMP",
+		"description" => "SNMP wizard aids in setting up the default SNMP settings of Cacti.",
+		"include" => "include/wizards/settings_snmp.php"
+		),
+	"paths" => array(
+		"friendly_name" => "Paths",
+		"description" => "Paths wizard aids in setting up the paths to required executables and logs for cacti.",
+		"include" => "include/wizards/settings_paths.php"
+		),
+	"poller" => array(
+		"friendly_name" => "Poller",
+		"description" => "Poller wizard aids in setting up the Poller Type, Poller Execution, Default Host Availability and Default Host Up/Down Settings.",
+		"include" => "include/wizards/settings_poller.php"
+		),
+	"visual" => array(
+		"friendly_name" => "Visual",
+		"description" => "Visual wizard aids in setting up the visual settings of Cacti, including the Default Theme, Rows per Page and other settings.",
+		"include" => "include/wizards/settings_visual.php"
+		),
 	"auth" => array(
 		"friendly_name" => "Authenication",
-		"description" => "Authenication wizard aids you in setting up Cacti's Authenication system for your enviroment.",
+		"description" => "Authenication wizard aids in setting up Cacti's Authenication system for your enviroment.  Be prepared to answer questions and have configuration settings that are appropriate to you enviroment.  If you plan on using LDAP, please have ready your LDAP server parameters, and if needed an authorzied binding for searching the LDAP server.",
 		"include" => "include/wizards/settings_auth.php"
 		),
-	"default" => "auth"
+	"graphexport" => array(
+		"friendly_name" => "Graph Export",
+		"description" => "Graph Export wizard aids in setting up exporting of graphs to static pages, either locally or FTP to a remote server.",
+		"include" => "include/wizards/settings_graphexport.php"
+		),
+	"default" => "general",
+	"intro" => "<b>Welcome to the Settings Wizard</b><br><br>This wizard is designed to help you setup features in Cacti.  Please select the section you would like to setup.",
+	"title" => "Setup Wizard"
 	);
 
 
 
-/* set default action */
-if (!isset($_REQUEST["action"])) { $_REQUEST["action"] = ""; }
-
-switch ($_REQUEST["action"]) {
-	case 'save':
-
-		break;
-	default:
-		default_menu();
-		break;
-}
-
-
-function default_menu() {
-
-	$value_array = array("auth" => "Authenication");
-
-
-	$form_array = array(
-		"wizard" => array(
-			"method" => "drop_array",
-			"friendly_name" => "Settings Section",
-			"description" => "Please select the settings wizard you would like to run.",
-			"value" => "",
-			"array" => $value_array
-			),
-		"description" => array(
-			"method" => "",
-			"friendly_name" => "",
-			"description" => "",
-			"default" => "boo",
-			"value" => ""
-			)
-		);
-
+if (isset($_REQUEST["wizard"])) {
 
 	include_once("include/top_header.php");
 
-	html_start_box("<strong>Settings Wizard</strong>", "98%", $colors["header_background"], "3", "center", "");
-
-	draw_edit_form( array( "fields" => $form_array ) );
-
-
-
-	html_end_box();
+	include_wizard($_REQUEST["wizard"]);
 
 	include_once("include/bottom_footer.php");
 
+}else{
+	include_once("include/top_header.php");
+
+	intro();
+	
+	include_once("include/bottom_footer.php");
+}
+
+/* 
+#########################################
+# Functions
+#########################################
+*/
+
+function include_wizard($wizard) {
+	global $wizard_array;
+	
+	$file = $wizard_array[$wizard]["include"];
+	
+	if (file_exists($file)) {
+		include_once($file);
+	}else{
+		display_custom_error_message("Unable to include Wizard File for wizard \"" . $wizard . "\"");
+	}
 
 }
 
+function intro() {
+	global $wizard_array,$colors;
 
+	/* create html and javascript lists for use in the html output */
+	$javascript = "";
+	$html = "";
+	while (list($field_name, $field_array) = each($wizard_array)) {
+		if (($field_name != "default") && ($field_name != "intro") && ($field_name != "title")) {
+			if ($wizard_array["default"] == $field_name) {
+				$html .= "\t\t\t\t\t\t\t<input type='radio' name='wizard' value='" . $field_name . "' checked onClick='applyDescription(this.value)'>" . $field_array["friendly_name"] . "<br>\n";
+			}else{
+				$html .= "\t\t\t\t\t\t\t<input type='radio' name='wizard' value='" . $field_name . "' onClick='applyDescription(this.value)'>" . $field_array["friendly_name"] . "<br>\n";
+			}
+			$javascript .= "  arrayValues['" . $field_name . "'] = '" . str_replace("'","\\'",$field_array["description"]) . "';\n";
+		}
+	}
 
+	/* javascript function to change decription box */
+	print "\n";
+	print "<script type=\"text/javascript\">\n";
+	print "<!--\n";
+	print "function applyDescription(strIndex) {\n";
+	print "  var arrayValues = new Array();\n";
+	print $javascript;
+	print "  obj = document.getElementById('wizardarea');\n";
+	print "  obj.innerHTML = arrayValues[strIndex];\n";
+	print "}\n";
+	print "-->\n";
+	print "</script>\n";
 
+	/* html output */
+	print "\t<form method='POST'>\n";
+
+	html_start_box("<strong>" . $wizard_array["title"] . "</strong>", "70%", $colors["header_background"], "3", "center", "");
+
+	print "\t\t\t\t\t<tr>\n";
+	print "\t\t\t\t\t\t<td colspan='5' bgcolor='" . $colors["form_alternate1"] . "' class='textArea'><br><blockquote>";
+	print $wizard_array["intro"];
+	print "</blockquote><br><hr noshade></td>\n";
+	print "\t\t\t\t\t</tr>\n"; 
+	print "\t\t\t\t\t<tr>\n";
+	print "\t\t\t\t\t\t<td width='15' valign='top' bgcolor='" . $colors["form_alternate1"] . "' class='textArea'></td>\n";
+	print "\t\t\t\t\t\t<td width='25%' valign='top' bgcolor='" . $colors["form_alternate1"] . "' class='textArea'><b> Section:</b><br><br>\n";
+	print $html;
+	print "\t\t\t\t\t\t</td>\n";
+	print "\t\t\t\t\t\t<td width='15' valign='top' bgcolor='" . $colors["form_alternate1"] . "' class='textArea'></td>\n";
+	print "\t\t\t\t\t\t<td valign='top' bgcolor='" . $colors["form_alternate1"] . "' class='textArea'><b>Description:</b><br><br>\n";
+	print "\t\t\t\t\t\t\t\t<div id='wizardarea' style='height:150px; overflow:auto;'>None</div><br>\n";
+	print "\t\t\t\t\t\t</td>\n";
+	print "\t\t\t\t\t\t<td width='15' valign='top' bgcolor='" . $colors["form_alternate1"] . "' class='textArea'></td>\n";
+	print "\t\t\t\t\t</tr>\n";
+
+	html_end_box();
+
+	print "\n\t<table align='center' width='70%' style='background-color: " . $colors['buttonbar_background'] . "; border: 1px solid #" . $colors["buttonbar_border"] . ";'>\n";
+	print "\t\t<tr>\n";
+	print "\t\t\t<td bgcolor='" . $colors['buttonbar_background'] . "' align='right'>\n";
+	print "\t\t\t\t<input type='image' src='" . html_get_theme_images_path("button_next.gif") . "' alt='Save' align='absmiddle'>\n";
+	print "\t\t\t</td>\n";
+	print "\t\t</tr>\n";
+	print "\t</table>\n";
+	print "\t</form>\n";
+
+	/* javascript function to change description area to default */
+	print "<script type=\"text/javascript\">\n";
+	print "<!--\n";
+	print "applyDescription('" . $wizard_array["default"] . "');\n";
+	print "-->\n";
+	print "</script>\n";
+
+}
 
 
 
