a7752c75810ea4cfa34229ec27c663b13bcd52f7
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2004-12-23 03:01:28 +0000

    small change for multi-pollers

--- trunk/cacti/cacti.sql
+++ trunk/cacti/cacti.sql
@@ -1876,7 +1876,7 @@ INSERT INTO graph_tree_items VALUES (7,1,0,0,'',1,'00100000000000000000000000000
 
 CREATE TABLE host (
   id mediumint(8) unsigned NOT NULL auto_increment,
-  poller_id smallint(5) unsigned NOT NULL default '0',
+  poller_id smallint(5) unsigned NOT NULL default '1',
   host_template_id mediumint(8) unsigned NOT NULL default '0',
   description varchar(150) NOT NULL default '',
   hostname varchar(250) default NULL,
@@ -1913,7 +1913,7 @@ CREATE TABLE host (
 -- Dumping data for table `host`
 --
 
-INSERT INTO host VALUES (1,0,8,'Localhost','127.0.0.1','',1,'','',NULL,NULL,NULL,161,500,1,0,'',3,0,'0000-00-00 00:00:00','0000-00-00 00:00:00','',"99999.9","0.00000","0.00000","0.00000",0,0,"100.00000");
+INSERT INTO host VALUES (1,1,8,'Localhost','127.0.0.1','',1,'','',NULL,NULL,NULL,161,500,1,0,'',3,0,'0000-00-00 00:00:00','0000-00-00 00:00:00','',"99999.9","0.00000","0.00000","0.00000",0,0,"100.00000");
 
 --
 -- Table structure for table `host_graph`
