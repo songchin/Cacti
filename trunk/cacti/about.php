d417a9fa7a7581079e77b02a859f649cf59306cf
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2005-05-15 14:38:37 +0000

    gettext changes

--- trunk/cacti/about.php
+++ trunk/cacti/about.php
@@ -26,7 +26,7 @@ include("./include/config.php");
 include("./include/auth.php");
 include("./include/top_header.php");
 
-html_start_box("<strong>About Cacti</strong>", "98%", $colors["header_background"], "3", "center", "");
+html_start_box("<strong>" . _("About Cacti") . "</strong>", "98%", $colors["header_background"], "3", "center", "");
 ?>
 
 <tr>
@@ -47,43 +47,49 @@ html_start_box("<strong>About Cacti</strong>", "98%", $colors["header_background
 
 		<p><strong><?php echo _("Developer Thanks"); ?></strong><br>
 		<ul type="disc">
-			<li><a href="http://blyler.cc">Andy Blyler</a>, for ideas, code, and that much needed overall support
-			during really lengthy coding sessions.</li>
-			<li>Rivo Nurges, for that c-based poller that was talked so long about. This <em>really</em> fast poller
-			is what will enable Cacti to make its way into larger and larger networks.</li>
-			<li>Larry Adams, for providing insight, time, superb support, and personal sanity. I could not have pulled
-			off a release of this magnitude without your help.
+			<li><a href="http://blyler.cc"><?php echo _("Andy Blyler</a>, for ideas, code, and that much needed overall support
+				during really lengthy coding sessions.");?>
+			</li>
+			<li><?php echo _("Rivo Nurges, for that c-based poller that was talked so long about. This <em>really</em> fast poller
+				is what will enable Cacti to make its way into larger and larger networks.");?>
+			</li>
+			<li><?php echo _("Larry Adams, for providing insight, time, superb support, and personal sanity. I could not have pulled
+				off a release of this magnitude without your help.");?>
+			</li>
 		</ul>
 		</p>
 
-		<p><strong><?php echo _("Thanks"); ?></a></strong><br>
+		<p><strong><?php echo _("Thanks");?></a></strong><br>
 		<ul type="disc">
-			<li>A very special thanks to <a href="http://ee-staff.ethz.ch/~oetiker/">Tobi Oetiker</a>,
-				the creator of <a href="http://www.mrtg.org/">RRDTool</a> and the very popular
-				<a href="http://www.mrtg.org">MRTG</a></li>
-			<li>Brady Alleman, creator of NetMRG and
-				<a href="http://www.thtech.net">Treehouse Technolgies</a> for questions and ideas. Just
+			<li><?php echo _("A very special thanks to <a href='http://ee-staff.ethz.ch/~oetiker/'>Tobi Oetiker</a>,
+				the creator of <a href='http://www.mrtg.org/'>RRDTool</a> and the very popular
+				<a href='http://www.mrtg.org'>MRTG</a>");?>
+			</li>
+			<li><?php echo _("Brady Alleman, creator of NetMRG and
+				<a href='http://www.thtech.net'>Treehouse Technolgies</a> for questions and ideas. Just
 				as a note, NetMRG is a complete Network Monitoring solution also written in PHP/MySQL. His
-				product also makes use of RRDTool's graphing capabilities, I encourage you to check it out.</li>
-			<li>The users of Cacti! Especially anyone who has taken the time to create a bug report, or otherwise
-				help me fix a Cacti-related problem. Also to anyone who has purchased an item from my amazon.com
-				wishlist or donated money via Paypal.</li>
+				product also makes use of RRDTool's graphing capabilities, I encourage you to check it out.");?>
+			</li>
+			<li><?php echo _("The users of Cacti! Especially anyone who has taken the time to create a bug report,
+				or otherwise help me fix a Cacti-related problem. Also to anyone who has purchased an item from my
+				amazon.com wishlist or donated money via Paypal.");?>
+			</li>
 		</ul>
 		</p>
 
-		<p><strong><?php echo _("License"); ?></strong><br>
+		<p><strong><?php echo _("License");?></strong><br>
 
-		<p><?php echo _("Cacti is licensed under the GNU GPL:"); ?></p>
+		<p><?php echo _("Cacti is licensed under the GNU GPL:");?></p>
 
-		<p><tt><?php echo _("This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version."); ?></tt></p>
+		<p><tt><?php echo _("This program is free software; you can redistribute it and/or modify it under the terms of the
+			GNU General	Public License as published by the Free Software Foundation; either version 2 of the License, or
+			(at your option) any later version.");?>
+		</tt></p>
 
-<p><tt><?php echo _("This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details."); ?></tt></p>
+		<p><tt><?php echo _("This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+			without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+			GNU General Public License for more details.");?>
+		</tt></p>
 
 		<p><strong>Cacti Variables</a></strong><span style="font-family: monospace; font-size: 10px;"><br>
 		<strong><?php echo _("Cacti OS:"); ?></strong> <?php print $config["cacti_server_os"];?><br>
