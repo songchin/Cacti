3e97d42bf21b50d2c15433383827857dcd06ca2f
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2005-05-15 06:10:56 +0000

    gettext changes

---
+++ trunk/cacti/locales/update-mo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+for po in po/*.po
+do
+  LANG=`echo $po | sed 's#po/##;s#\.po$##'`
+  mkdir -p $LANG/LC_MESSAGES
+  echo -n "$po ... "
+  msgfmt -v --statistic -o $LANG/cacti.mo $po
+done
---
+++ trunk/cacti/locales/update-pot.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xgettext -k_ `find ../ -name \*.php` -o po/cacti.pot
