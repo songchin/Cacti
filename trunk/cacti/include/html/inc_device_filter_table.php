b4771bd2eadebfd313c665279a20136445032bc0
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2005-06-26 17:58:23 +0000

    change filtering method for devices

--- trunk/cacti/include/html/inc_device_filter_table.php
+++ trunk/cacti/include/html/inc_device_filter_table.php
@@ -1,3 +1,14 @@
+	<script type="text/javascript">
+	<!--
+	function applyFilterChange(objForm) {
+		strURL = '?host_template_id=' + objForm.host_template_id[objForm.host_template_id.selectedIndex].value;
+		strURL = strURL + '&host_status=' + objForm.host_status[objForm.host_status.selectedIndex].value;
+		strURL = strURL + '&filter=' + objForm.filter.value;
+		document.location = strURL;
+	}
+	-->
+	</script>
+
 	<tr bgcolor="<?php print $colors["filter_background"];?>">
 		<form name="form_devices">
 		<td>
@@ -7,15 +18,15 @@
 						<?php echo _("Type:");?>&nbsp;
 					</td>
 					<td width="1">
-						<select name="cbo_host_template_id" onChange="window.location=document.form_devices.cbo_host_template_id.options[document.form_devices.cbo_host_template_id.selectedIndex].value">
-							<option value="host.php?host_template_id=-1&host_status=<?php print $_REQUEST["host_status"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["host_template_id"] == "-1") {?> selected<?php }?>>Any</option>
-							<option value="host.php?host_template_id=0&host_status=<?php print $_REQUEST["host_status"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["host_template_id"] == "0") {?> selected<?php }?>>None</option>
+						<select name="host_template_id" onChange="applyFilterChange(document.form_devices)">
+							<option value="-1"<?php if ($_REQUEST["host_template_id"] == "-1") {?> selected<?php }?>>Any</option>
+							<option value="0"<?php if ($_REQUEST["host_template_id"] == "0") {?> selected<?php }?>>None</option>
 							<?php
 							$host_templates = db_fetch_assoc("select id,name from host_template order by name");
 
 							if (sizeof($host_templates) > 0) {
 							foreach ($host_templates as $host_template) {
-								print "<option value='host.php?host_template_id=" . $host_template["id"] . "&host_status=" . $_REQUEST["host_status"] . "&filter=" . $_REQUEST["filter"] . "&page=1'"; if ($_REQUEST["host_template_id"] == $host_template["id"]) { print " selected"; } print ">" . $host_template["name"] . "</option>\n";
+								print "<option value=" . $host_template["id"]; if ($_REQUEST["host_template_id"] == $host_template["id"]) { print " selected"; } print ">" . $host_template["name"] . "</option>\n";
 							}
 							}
 							?>
@@ -26,13 +37,13 @@
 						<?php echo _("Status:");?>&nbsp;
 					</td>
 					<td width="1">
-						<select name="cbo_host_status" onChange="window.location=document.form_devices.cbo_host_status.options[document.form_devices.cbo_host_status.selectedIndex].value">
-							<option value="host.php?host_status=-1&host_template_id=<?php print $_REQUEST["host_template_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["host_status"] == "-1") {?> selected<?php }?>>Any</option>
-							<option value="host.php?host_status=3&host_template_id=<?php print $_REQUEST["host_template_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["host_status"] == "3") {?> selected<?php }?>>Up</option>
-							<option value="host.php?host_status=-2&host_template_id=<?php print $_REQUEST["host_template_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["host_status"] == "-2") {?> selected<?php }?>>Disabled</option>
-							<option value="host.php?host_status=1&host_template_id=<?php print $_REQUEST["host_template_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["host_status"] == "1") {?> selected<?php }?>>Down</option>
-							<option value="host.php?host_status=2&host_template_id=<?php print $_REQUEST["host_template_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["host_status"] == "2") {?> selected<?php }?>>Recovering</option>
-							<option value="host.php?host_status=0&host_template_id=<?php print $_REQUEST["host_template_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["host_status"] == "0") {?> selected<?php }?>>Unknown</option>
+						<select name="host_status" onChange="applyFilterChange(document.form_devices)">
+							<option value="-1"<?php if ($_REQUEST["host_status"] == "-1") {?> selected<?php }?>>Any</option>
+							<option value="3"<?php if ($_REQUEST["host_status"] == "3") {?> selected<?php }?>>Up</option>
+							<option value="-2"<?php if ($_REQUEST["host_status"] == "-2") {?> selected<?php }?>>Disabled</option>
+							<option value="1"<?php if ($_REQUEST["host_status"] == "1") {?> selected<?php }?>>Down</option>
+							<option value="2"<?php if ($_REQUEST["host_status"] == "2") {?> selected<?php }?>>Recovering</option>
+							<option value="0"<?php if ($_REQUEST["host_status"] == "0") {?> selected<?php }?>>Unknown</option>
 						</select>
 					</td>
 					<td width="5"></td>
