d0a4c6378852379a30810c2e194c04a3cb2c1dd0
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2006-05-07 00:54:11 +0000

    missed change for internationalization fix

--- trunk/cacti/include/global.php
+++ trunk/cacti/include/global.php
@@ -58,6 +58,8 @@ $colors["form_background_dark"] = "E1E1E1";
 $colors["form_alternate1"] = "F5F5F5";
 $colors["form_alternate2"] = "E5E5E5";
 
+/* Implement internationalization support */
+require(CACTI_BASE_PATH . "/include/global_language.php");
 
 /* Contants includes */
 require(CACTI_BASE_PATH . "/include/global_constants.php");
@@ -190,63 +192,5 @@ if ((bool)ini_get("register_globals")) {
 
 /* display ALL errors */
 error_reporting(E_ALL);
-
-/* cacti language support */
-$lang2locale = array(
-	"en"=>"us_EN.UTF-8",
-	"ja"=>"ja_JP.UTF-8",
-	"fr"=>"fr_FR.iso-8859-1",
-	"sv"=>"sv_SE.iso-8859-1",
-	"es"=>"es_ES.iso-8859-1",
-	"bg"=>"bg_BG.cp1251"
-	);
-
-$locale = $lang2locale[$cacti_lang];
-setlocale(LC_ALL, $locale);
-putenv("LC_ALL=" . $locale);
-putenv("LANG=" . $cacti_lang);
-
-/* determine whether or not we need to emulate gettext */
-if (!function_exists("_")) {
-	require(CACTI_BASE_PATH . "/include/gettext/streams.php");
-	require(CACTI_BASE_PATH . "/include/gettext/gettext.php");
-
-	$locale_path = $config["base_path"] . "/locales/" . $cacti_lang . "/LC_MESSAGES/" . "cacti.mo";
-	if ((!file_exists($locale_path)) && ($cacti_lang != "en")) {
-		die("Cacti language locale file not found.  Please locate your language file and then you can continue.");
-	}
-
-	if ($cacti_lang != "en") {
-		$input = new FileReader($locale_path);
-		$l10n = new gettext_reader($input);
-
-		/* create standard wrapers, so gettext functions can work */
-		function _($text) {
-			global $l10n;
-			return $l10n->translate($text);
-		}
-
-		function _ngettext($single, $plural, $number) {
-			global $l10n;
-			return $l10n->ngettext($single, $plural, $number);
-		}
-	} else {
-		function _($text) {
-			return $text;
-		}
-
-		function _ngettext($single, $plural, $number) {
-			if ($number == 1) {
-				return $single;
-			} else {
-				return $plural;
-			}
-		}
-	}
-} else {
-	bindtextdomain("cacti", CACTI_BASE_PATH . "/locales");
-	textdomain("cacti");
-	bind_textdomain_codeset ("cactid", "UTF-8");
-}
 
 ?>
