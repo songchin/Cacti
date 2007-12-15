e276b1055d9261185b48b7f036a244f74a33aab1
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2007-12-15 21:51:18 +0000

    feature#0001123: Performance tweak by adding some indices

---
+++ cacti/branches/main/install/0_8_7a_to_0_8_8.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2007 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+function upgrade_to_0_8_8() {
+	/* speed up the UI */
+	db_install_execute("0.8.8", "ALTER TABLE `graph_templates_item` ADD INDEX `task_item_id`(`task_item_id`)");
+	db_install_execute("0.8.8", "ALTER TABLE `data_local` ADD INDEX `host_id`(`host_id`)");
+}
+?>
