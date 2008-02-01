a0102ee52eadae1249fac6626025621c6c43e22f
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2008-02-01 16:17:21 +0000

    moved an additional index from 088 upgrade script to 087b upgrade script.

--- cacti/branches/main/install/0_8_7a_to_0_8_7b.php
+++ cacti/branches/main/install/0_8_7a_to_0_8_7b.php
@@ -27,5 +27,7 @@ function upgrade_to_0_8_7b() {
 	db_install_execute("0.8.7b", "ALTER TABLE `graph_templates_item` ADD INDEX `task_item_id` ( `task_item_id` )");
 	/* make CLI more responsive */
 	db_install_execute("0.8.7b", "ALTER TABLE `data_input_data` ADD INDEX `t_value`(`t_value`)");
+	/* speed up the UI */
+	db_install_execute("0.8.7b", "ALTER TABLE `data_local` ADD INDEX `host_id`(`host_id`)");
 }
 ?>
