4d94b968b6cbfb8b78f5c5fb9698ccf0460b36e6
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2008-02-08 17:25:59 +0000

    fixed typo (missing /) for file inclusion

--- cacti/branches/main/lib/html_tree.php
+++ cacti/branches/main/lib/html_tree.php
@@ -641,7 +641,7 @@ function grow_right_pane_tree($tree_id, $leaf_id, $host_group_data) {
 	/* include time span selector */
 	if (read_graph_config_option("timespan_sel") == "on") {
 		html_graph_start_box(3, false);
-		include($config["include_path"] . "html/inc_timespan_selector.php");
+		include($config["include_path"] . "/html/inc_timespan_selector.php");
 		html_graph_end_box();
 	}
 
