7c2212ca4df671abfde39f8e41a420c7a82c92c1
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2008-02-19 02:02:17 +0000

    bug#0001182: Subdirectories are not secure and allow browsing bug#0001085: LDAP enabled status not maintained on upgrade from 0.8.6j to 0.8.7

--- cacti/branches/main/install/0_8_6j_to_0_8_7.php
+++ cacti/branches/main/install/0_8_6j_to_0_8_7.php
@@ -71,13 +71,11 @@ function upgrade_to_0_8_7() {
 	$global_auth_db = db_fetch_row("SELECT value FROM settings WHERE name = 'global_auth'");
 	if (sizeof($global_auth_db)) {
 		$global_auth = $global_auth_db["value"];
-
 	}
-        $ldap_enabled = "";
+	$ldap_enabled = "";
 	$ldap_enabled_db = db_fetch_row("SELECT value FROM settings WHERE name = 'ldap_enabled'");
 	if (sizeof($ldap_enabled_db)) {
 		$ldap_enabled = $ldap_enabled_db["value"];
-
 	}
 	if ($global_auth == "on") {
 		if ($ldap_enabled == "on") {
