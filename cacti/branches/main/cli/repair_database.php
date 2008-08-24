09c35ff0255c32b3de8271b4a015049cfa6572e7
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2008-08-24 12:46:26 +0000

    Some tweaks for including files (just to match other cli scripts)

--- cacti/branches/main/cli/repair_database.php
+++ cacti/branches/main/cli/repair_database.php
@@ -30,7 +30,7 @@ if (!isset($_SERVER["argv"][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($
 
 $no_http_headers = true;
 
-include("../include/global.php");
+include(dirname(__FILE__) . "/../include/global.php");
 
 /* process calling arguments */
 $parms = $_SERVER["argv"];
