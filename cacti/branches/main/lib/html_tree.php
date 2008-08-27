998221f6b29f7a726c97a2f5caecd22c0cbceceb
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2008-08-27 20:09:31 +0000

    QA on timespan_selector include

--- cacti/branches/main/lib/html_tree.php
+++ cacti/branches/main/lib/html_tree.php
@@ -55,7 +55,7 @@ function grow_graph_tree($tree_id, $start_branch, $user_id, $options) {
 	/* include time span selector */
 	if (read_graph_config_option("timespan_sel") == "on") {
 		html_graph_start_box(3, false);
-		include(CACTI_BASE_PATH . "/include/html/inc_timespan_selector.php");
+		include(CACTI_BASE_PATH . "/lib/timespan_selector.php");
 		html_graph_end_box();
 	}
 
@@ -667,7 +667,7 @@ function grow_right_pane_tree($tree_id, $leaf_id, $host_group_data) {
 	/* include time span selector */
 	if (read_graph_config_option("timespan_sel") == "on") {
 		html_graph_start_box(3, false);
-		include(CACTI_BASE_PATH . "/include/html/inc_timespan_selector.php");
+		include(CACTI_BASE_PATH . "/lib/timespan_selector.php");
 		html_graph_end_box();
 	}
 
