26d53f3aba6b66d259dd6074238ffb83328d050e
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2008-09-21 07:12:18 +0000

    -bug: IE generates errors on both the host and graph settings pages as well as firefox in some conditions on graph pages.

--- cacti/branches/main/graph_settings.php
+++ cacti/branches/main/graph_settings.php
@@ -182,7 +182,21 @@ function settings() {
 		}
 	}
 
-	window.onload = graphSettings();
+	function addLoadEvent(func) {
+		var oldonload = window.onload;
+		if (typeof window.onload != 'function') {
+			window.onload = func;
+		} else {
+			window.onload = function() {
+				if (oldonload) {
+					oldonload();
+				}
+				func();
+			}
+		}
+	}
+
+	addLoadEvent(graphSettings);
 
 	-->
 	</script>
