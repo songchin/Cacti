bdfc6da10a06760cb64e3990f8afecfa968022db
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2008-12-15 20:29:39 +0000

    array_merge seems to reindex the resulting array to base index 0. So use array1 + array2 instead. This preserves the indices

--- cacti/branches/main/lib/form_host.php
+++ cacti/branches/main/lib/form_host.php
@@ -1474,7 +1474,7 @@ function host() {
 	html_end_box(false);
 
 	/* add a list of tree names to the actions dropdown */
-	$device_actions = array_merge($device_actions, api_tree_add_tree_names_to_actions_array());
+	$device_actions = $device_actions + api_tree_add_tree_names_to_actions_array();
 
 	/* draw the dropdown containing a list of available actions for this form */
 	draw_actions_dropdown($device_actions);
