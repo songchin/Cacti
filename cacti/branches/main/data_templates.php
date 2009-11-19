9531984107905e91a626d9ec82619f091fad4ee4
pasnak <pasnak@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2009-11-19 21:54:15 +0000

    fix missing bracket

--- cacti/branches/main/data_templates.php
+++ cacti/branches/main/data_templates.php
@@ -329,7 +329,7 @@ function form_actions() {
 
 	/* loop through each of the graphs selected on the previous page and get more info about them */
 	while (list($var,$val) = each($_POST)) {
-		if (preg_match("/^chk_([0-9]+)$/", $var, $matches))
+		if (preg_match("/^chk_([0-9]+)$/", $var, $matches)) {
 			/* ================= input validation ================= */
 			input_validate_input_number($matches[1]);
 			/* ==================================================== */
