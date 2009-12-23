e5f9329f97d86e417143e520c1452e7120f80950
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2009-12-23 22:12:46 +0000

    print color codes in inverse with respect to the background-color

--- cacti/branches/main/lib/jquery/colorpicker.js
+++ cacti/branches/main/lib/jquery/colorpicker.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
 
 	// set background color before page load
 	$('.colortags').each(function() {
-		$(this).css('background-color', '#' + this.value).attr('title', this.value);
+		var background_color_hex = this.value;
+		var color_hex = invertColor(background_color_hex);
+		$(this).css('background-color', '#' + background_color_hex).attr('title', background_color_hex).css('color', color_hex);
 	});
 
 	$('.colortags').ColorPicker({
@@ -24,12 +26,23 @@ $(document).ready(function(){
 		onSubmit: function(hsb, hex, rgb, el) {
 			$(el).val(hex);
 			$(el).ColorPickerHide();
-			$(el).css('background-color', '#' + hex).attr('title', hex);
+			var color_hex = invertColor(hex);
+			$(el).css('background-color', '#' + hex).attr('title', hex).css('color', color_hex);
 		}
 	})
 	.bind('keyup', function() {
 		$(this).ColorPickerSetColor(this.value).attr('title', this.value);
 	});
+	
+	function invertColor(color_hex) {
+		var result;
+		if (result = /([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(color_hex)) {
+			var red = 255 - parseInt(result[1],16);
+			var green = 255 - parseInt(result[2],16);
+			var blue = 255 - parseInt(result[3],16);
+			return 'rgb('+red+','+green+','+blue+')';
+		}
+	}
 });
 //-->
 </script>
