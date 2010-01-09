5a667a47a6a0a0b6b4c7cdb9c11b853cd3221ed8
cigamit <cigamit@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2010-01-09 20:23:19 +0000

    Fix the ability to create new graphs.  Honestly not sure why we are using 2 different variables here.

--- cacti/branches/main/devices.php
+++ cacti/branches/main/devices.php
@@ -51,7 +51,7 @@ if (!isset($_REQUEST["action"])) { $_REQUEST["action"] = ""; }
 
 switch (get_request_var_request("action")) {
 	case 'save':
-		if (isset($_REQUEST["save_component_graph"])) {
+		if (isset($_REQUEST["save_component_graph"]) || isset($_REQUEST["save_component_new_graphs"])) {
 			api_graphs_new_form_save();
 		}else{
 			api_device_form_save();
