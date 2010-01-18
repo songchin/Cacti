7a903b6f818fc1c09cf296b045e1b6e1d86c5e27
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2010-01-18 18:39:51 +0000

    QA: Rework some file descriptor logic

--- spine/branches/main/poller.c
+++ spine/branches/main/poller.c
@@ -1332,13 +1332,14 @@ char *exec_poll(device_t *current_device, char *command) {
 	#endif
 
 	if (cmd_fd > 0) {
+		retry:
+
 		/* Initialize File Descriptors to Review for Input/Output */
 		FD_ZERO(&fds);
 		FD_SET(cmd_fd, &fds);
 
 		/* wait x seonds for pipe response */
-		retry:
-		switch (select(FD_SETSIZE, &fds, NULL, NULL, &timeout)) {
+		switch (select(cmd_fd+1, &fds, NULL, NULL, &timeout)) {
 		case -1:
 			switch (errno) {
 			case EBADF:
@@ -1385,7 +1386,7 @@ char *exec_poll(device_t *current_device, char *command) {
 			close_fd = FALSE;
 			#else
 			pid = nft_pchild(cmd_fd);
-			kill(pid, SIGTERM);
+			kill(pid, SIGKILL);
 			#endif
 
 			SET_UNDEFINED(result_string);
